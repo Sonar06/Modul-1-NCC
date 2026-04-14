8c78405611d3d2a8fc26f8d41ad2691e4ea4a9cd
Sonar06 <roki.kcm@gmail.com>
2026-04-15 00:34:15 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({
+	"nama": "Khairan Cherokee Musthfoa",
+        "nrp": "5025241215",
+        "status": "UP",
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
