bd2780bb51003e406551bfd0b2c13184bf272984
Cherokee <roki.kcm@gmail.com>
2026-05-09 17:41:31 +0700

    feat: testing

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "mysql-db";
+$host = "mysql-db";     // Sesuai container_name di docker-compose
 $user = "root";
-$pass = "root";
-$db   = "iniberita";
+$pass = "rootpass";     // SESUAIKAN: tadi di docker-compose kamu pakai 'rootpass'
+$db   = "iniberita";    // SESUAIKAN: tadi di docker-compose kamu pakai 'iniberita'
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
